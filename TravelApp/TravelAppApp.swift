b83ee0501e9a73c772835bf182c427dbc222e13f
Alex Baluhtin <aliakseibalukhtsin24@gmail.com>
2023-03-29 09:13:01 +0200

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Alex Balukhtsin on 29.03.23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
